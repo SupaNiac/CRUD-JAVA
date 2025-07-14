2a5419c940659eb78a24b698285eebf95ad7ee3e
Luiz Henrique <luiz04faria@gmail.com>
2025-07-14 18:17:50 -0300

    Criado

---
+++ src/main/java/model/Usuario.java
@@ -0,0 +1,28 @@
+package model;
+
+public class Usuario {
+
+    String Email;
+    String Senha;
+
+    public Usuario(String senha, String email) {
+        Senha = senha;
+        Email = email;
+    }
+
+    public String getEmail() {
+        return Email;
+    }
+
+    public void setEmail(String email) {
+        Email = email;
+    }
+
+    public String getSenha() {
+        return Senha;
+    }
+
+    public void setSenha(String senha) {
+        Senha = senha;
+    }
+}
